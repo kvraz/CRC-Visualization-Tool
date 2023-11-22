3036cc91074be2b93f68d93984eba621503ff9be
kvraz <kostas.vrazalis@gmail.com>
2023-11-23 01:25:17 +0200

    continued js

--- crcvistool.js
+++ crcvistool.js
@@ -1,6 +1,6 @@
 function getValues() {
-    const messageValue = document.getElementById('message').value;
-    const polynomialValue = document.getElementById('polynomial').value;
+    const messageValue = document.getElementById('message').value.trim().replace(/\s/g, '');;
+    const polynomialValue = document.getElementById('polynomial').value.trim();
 
     return {
         message: messageValue,
@@ -8,13 +8,13 @@ function getValues() {
     };
 }
 
+
 function isBinary(message) {
-    return /^[01]+$/.test(message); // Regex to check if the string contains only 0s and 1s
+    return /^[01\s]+$/.test(message.replace(/\s/g, '')); // Ignore spaces in the test
 }
 
 function isValidPolynomial(polynomial) {
-    // Regular expression to match the polynomial format: x^k + x^(k-1) + ... + 1
-    return /^x\^\d+(\s*\+\s*x\^\d+)*(\s*\+\s*1)?$/i.test(polynomial);
+    return /^x\s*\^\s*\d+(\s*\+\s*x\s*\^\s*\d+)*(\s*\+\s*1)?$/i.test(polynomial.replace(/\s/g, ''));
 }
 
 function convertToBinary(polynomial) {
@@ -32,17 +32,49 @@ function convertToBinary(polynomial) {
 
     const maxExponent = Math.max(...matches); // Find the maximum exponent
 
-    let binaryString = '';
+    let binaryPolynomial = '';
     for (let i = maxExponent; i > 0; i--) {
-        binaryString += matches.includes(i) ? '1' : '0'; // Check if the exponent exists in matches array
+        binaryPolynomial += matches.includes(i) ? '1' : '0'; // Check if the exponent exists in matches array
     }
 
     let lastLetter = polynomial.charAt(polynomial.length-1);
-    if (lastLetter == 1 && polynomial.charAt(polynomial.length-2) != '^') binaryString += '1';
-    else binaryString += '0';
+    if (lastLetter == 1 && polynomial.charAt(polynomial.length-2) != '^') binaryPolynomial += '1';
+    else binaryPolynomial += '0';
 
-    return binaryString;
+    return binaryPolynomial;
 }
+
+function bitwiseXOR(str1, str2) {
+    const minLength = Math.min(str1.length, str2.length);
+    let result = '';
+    
+    for (let i = 0; i < minLength; i++) {
+        if (str1.charAt(i) !== str2.charAt(i)) {
+            result += '1';
+        } else {
+            result += '0';
+        }
+    }
+
+    return result;
+}
+function calculateRemainder(message, polynomial){
+    let result = bitwiseXOR(message, polynomial);
+    
+
+    // Trim leading zeros from the result
+    result = result.replace(/^0+/, '');
+    for (let j = 0; j<=(polynomial.length - result.length); j++) result += message.charAt(j + polynomial.length);
+
+    let newmessage = result;
+
+    for(let i=result.length; i<message.length; i++) newmessage += message.charAt(i);
+
+    console.log(newmessage); //this is the result with the rest of the message's bits. xor must be perfomed to get next remainder
+    return result;
+
+}
+let calculationDone = false; // Flag to track if initial calculation has been done
 
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission for demonstration
@@ -72,15 +104,46 @@ document.getElementById('form').addEventListener('submit', function(event) {
     polynomialLine.textContent = `G(x) = ${convertToBinary(polynomial)}`;
     newLine.textContent = `M(x) * x^${polynomialGrade} = ${newMessage} `;
 
+    calculationDone = true; // Set the flag to indicate initial calculation done
 });
 
 document.getElementById('form').addEventListener('reset', function() {
-    const errorMessage = document.getElementById('errorMessage');
+    const messageLine = document.getElementById('messageValue');
     const polynomialLine = document.getElementById('polynomialValue');
     const newLine = document.getElementById('newValue');
+    const errorMessage = document.getElementById('errorMessage');
 
-    errorMessage.textContent = ''; // Clear error message when the form is reset
-    messageLine.textContent = '';
-    polynomialLine.textContent = ''; // Clear polynomial display when the form is reset
-    newLine.textContent = '';
+    // Clear fields message when the form is reset
+    errorMessage.textContent = ''; 
+    messageLine.textContent = 'M(x) =';
+    polynomialLine.textContent = 'G(x) ='; 
+    newLine.textContent = 'M(x) * x^k = ';
 });
+
+document.querySelector('.values-box input[type="submit"]').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent form submission for demonstration
+    
+    if (!calculationDone) {
+        // Prevent "Calculate Remainder" if initial calculation hasn't been done
+        return;
+    }
+
+    const { message, polynomial } = getValues();
+    const remainderContainer = document.getElementById('remainderContainer');
+
+    let binaryPolynomial = convertToBinary(polynomial)
+    let polynomialGrade = convertToBinary(polynomial).length - 1;
+    let newMessage = message;
+    for (let i = 0; i < convertToBinary(polynomial).length - 1; i++) {
+        newMessage += '0';
+    }
+
+    // Display the newMessage and the polynomial in the remainder container
+    remainderContainer.innerHTML = `
+        <p>M(x) * x^${polynomialGrade} = ${newMessage}<br>
+        G(x) =  ${binaryPolynomial}</p>
+    `;
+    
+    console.log(calculateRemainder(newMessage, binaryPolynomial))
+});
+
